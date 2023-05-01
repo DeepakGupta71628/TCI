d17dfe81e0f0ffef86ab7710ac39cd752d8eee5a
Deepak Gupta <deepakgupta71628@gmail.com>
2023-05-01 22:08:05 +0530

    tci bonus app created

---
+++ bonusApp/src/test/java/com/tci/bonusApp/BonusAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tci.bonusApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonusAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
